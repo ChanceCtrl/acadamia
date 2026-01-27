81ccdbc9dd010bc78adc1de690d6a563539c5c29
ChanceCtrl <142356358+ChanceCtrl@users.noreply.github.com>
2026-01-27 12:13:07 -0500

    mrow

---
+++ cpe3500/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "STM32 dev shell with ARM GCC toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc-arm-embedded
+          cmake
+          ninja
+          gdb
+          openocd
+          python3
+        ];
+
+        shellHook = ''
+          echo "STM32 dev shell ready"
+          echo "Using compiler: $(which arm-none-eabi-gcc)"
+        '';
+      };
+    };
+}
